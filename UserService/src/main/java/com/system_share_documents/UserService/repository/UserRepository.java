815292d25fc2666fe6d241295fa39f0f5bd93416
leetuan0342 <leetuan0342@gmail.com>
2025-09-17 21:24:50 +0700

    add keycloak

---
+++ UserService/src/main/java/com/system_share_documents/UserService/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.system_share_documents.UserService.repository;
+
+import com.system_share_documents.UserService.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
